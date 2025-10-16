df3a5d55b0dd70483a111163702f0217a34602b2
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-10-16 15:42:41 +0100

    FFHQ (training , model changes)

--- README.md
+++ README.md
@@ -1,23 +1,15 @@
 ### This Repository is Style GAN in PyTorch , From this paper : [A Style-Based Generator Architecture for Generative Adversarial Networks](https://arxiv.org/abs/1812.04948) 
-### Data : Caleba Dataset (with 10k subset) 
+### Data : FFHQ
 
-### in epoch 10 / 300 
-![image alt](https://github.com/HajarHAMDOUCH01/Face-Generator-StyleGAN-PyTorch/blob/b68a476369d4d2e698b880b498a803eb3028515d/samples_epoch_10%20(1).png)
-
-### in epoch 15 / 300 
-![image alt](https://github.com/HajarHAMDOUCH01/Face-Generator-StyleGAN-PyTorch/blob/b68a476369d4d2e698b880b498a803eb3028515d/samples_epoch_15%20(3).png)
-
-
-To decrease training time and prove model functionning , I :
-- Image size : 128*128
-- Subset of 10k images from CalebA dataset
-- Because the subset was smaller , 8 layers of fully connected layers of mapping net will make the model memorize a space W of the data too well , like using too much effort while 10k is not worth that much effort. so I changed mapping net depth from 8 (as the paper) to 4 because i saw mode collapse in epoch 40 / 300.
+### in epoch 26 / 100 
+![image alt](https://github.com/HajarHAMDOUCH01/Face-Generator-StyleGAN-PyTorch/blob/d450b4268bd570c20df92cfe0290410c00d4c02d/samples_2.png)
 
 ### Inference :
 - Run testing/test.py , it geneartes a new face using scripted model using Torchscript.
 
 ### Model weights :
-- i will upload best .pth files in a checkpoint in hugging face for fine tunning or inference. 
+Model will be in hugging face, ready to use via transformers.
 
 ### stay tuned for a web app for face generation.
+
 
--- testing/test.py
+++ testing/test.py
@@ -3,25 +3,50 @@ from torch import Tensor
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 import sys
 sys.path.append("/content/Face-Generator-StyleGAN-PyTorch")
-from style_gan import StyleGAN
+from model.style_gan import StyleGAN
 from training_config import training_config
-
-config = {
-        "z_dim" : training_config["z_dim"],
-        "w_dim" : training_config["w_dim"],
-        "img_size" : training_config["img_size"],
-        "img_channels" : training_config["img_channels"],
-        "mapping_layers" : training_config["mapping_layers"],
-        "style_mixing_prob" : training_config["style_mixing_prob"]}
+import matplotlib.pyplot as plt
+import os
 
 
-def load_model(model_weights_path=""):
+def generate_samples(config, model_weights_path=""):
     model = torch.load(model_weights_path, map_location=device)
-    style_gan = StyleGAN(**config)
-    style_gan.load_state_dict(model)
-    generator = style_gan.generator
-    generator.load_state_dict(model.synthe)
+    img_size = config["image_size"]
+    z_dim = config["z_dim"]
+    w_dim = config["w_dim"]
+    batch_size = config["batch_size"]
+    num_epochs = config["num_epochs"]
+    save_dir = config["save_dir"]
+    os.makedirs(save_dir, exist_ok=True)
+    
+    generator = StyleGAN(
+        z_dim=z_dim,
+        w_dim=w_dim,
+        img_size=img_size,
+        img_channels=3,
+        mapping_layers=config["mapping_layers"],
+        style_mixing_prob=config["style_mixing_prob"]
+    ).to(device)
+    generator.load_state_dict(model["generator_state_dict"])
+    generator.eval()
+    with torch.no_grad():
+        z = torch.randn(8, z_dim, device=device)
+        samples = generator.generate(z, truncation_psi=1.0)
+        
+        # Denormalize from [-1, 1] to [0, 1]
+        samples = (samples + 1) / 2
+        samples = torch.clamp(samples, 0, 1)
+        
+        import torchvision.utils as vutils
+        grid = vutils.make_grid(samples, nrow=4, padding=2, normalize=False)
+        
+        plt.figure(figsize=(10, 10))
+        plt.imshow(grid.permute(1, 2, 0).cpu())
+        plt.axis('off')
+        plt.title(f'Generated Samples')
+        save_path = os.path.join("/content/", f'samples_test.png')
+        plt.savefig(save_path, dpi=150, bbox_inches='tight')
+        plt.close()
 
-def generate_face(z: Tensor, ) -> Tensor:
-    z = torch.randn((1,512), device=device)
-    w = 
+if __name__ == "__main__":
+    generate_samples(training_config, model_weights_path="/content/models--hajar001--StyleGAN-Caleba/snapshots/b0ea56a0375134ce5071765dc22b580f621f0f8b/stylegan_checkpoint_epoch_14.pth")
