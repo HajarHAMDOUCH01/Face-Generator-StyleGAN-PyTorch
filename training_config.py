2ad482c2eb6a8de762d23e398cb2c86070846385
HajarHAMDOUCH01 <hajar.hamdouch02@gmail.com>
2025-10-16 19:02:10 +0100

    config

---
+++ training_config.py
@@ -0,0 +1,41 @@
+training_config = {
+    # Architecture
+    "image_size": 128,
+    "z_dim": 512,
+    "w_dim": 512,
+    "mapping_layers": 8,
+    "style_mixing_prob": 0.8,
+    
+    # Dataset
+    "dataset_path": "/content/ffhq_128x128_jpg",  
+    "dataset_limit": 70000,
+    
+    # Training
+    "batch_size": 40,
+    "num_epochs": 100,
+    "num_workers": 8,
+    "prefetch_factor": 4,
+    
+    # Optimizer
+    "adam_beta1": 0.0,
+    "adam_beta2": 0.99,
+    "adam_eps": 1e-8,
+    "g_lr": 0.00025,
+    "d_lr": 0.00020,
+    
+    # Regularization
+    "plr_weight": 2.0,
+    "plr_interval": 4,
+    "plr_decay": 0.01,
+    "r1_gamma": 5.0,
+    "r1_interval": 16,
+    "warmup_epochs": 5,
+    
+    # Logging
+    "save_dir": "/content/drive/MyDrive/stylegan_checkpoints",
+    "save_every": 1,
+    "sample_every": 1,
+    "log_every": 100,
+    
+    "use_hf_upload": True,  
+}
